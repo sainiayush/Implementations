3472151b373ec7463a9bb39e5a9e44b1aa53652c
Ayush Saini <76840697+sainiayush@users.noreply.github.com>
2023-03-11 19:05:36 +0530

    Update dsu.java

--- DSU/dsu.java
+++ DSU/dsu.java
@@ -1,15 +1,24 @@
 class DSU {
   private int[] par;
   private int[] rnk;
+  private int[] sz;
   private int c;
   public DSU(int n) {
       par = new int[n+1];
       rnk = new int[n+1];
+      sz = new int[n+1];
       c=n;
-      for (int i = 1; i <= n; ++i) par[i] = i;
+      sz[0]=1;
+      for (int i = 1; i <= n; ++i) {
+        par[i] = i;
+        sz[i] = 1;
+      }
   }
   public int find(int i) {
     return (par[i] == i ? i : (par[i] = find(par[i])));
+  }
+  public int get_size(int i){
+    return sz[find(i)];
   }
   public boolean same(int i, int j) {
     return find(i) == find(j);
@@ -27,6 +36,7 @@ class DSU {
     else --c;
     if (rnk[i] > rnk[j]) swap(i, j);
     par[i] = j;
+    sz[j] += sz[i];
     if (rnk[i] == rnk[j]) rnk[j]++;
     return true;
   }
